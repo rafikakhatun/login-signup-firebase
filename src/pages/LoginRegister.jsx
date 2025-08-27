cfa7e02f81616a76fd5437ec0930b0a6fd7326a8
rafika khatun <rafikakhatun607@gmail.com>
2025-08-27 22:48:58 +0530

    Initialize LoginRegister component structure

--- src/pages/LoginRegister.jsx
+++ src/pages/LoginRegister.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const LoginRegister = () => {
     return (
-        <div>
-            hjjj
-        </div>
+       <>
+       
+       
+       
+       </>
     );
 };
 
