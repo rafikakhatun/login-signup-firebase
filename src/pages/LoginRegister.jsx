59ff118cf6cde28478e342604f20e79003c7f37a
rafika khatun <rafikakhatun607@gmail.com>
2025-08-28 22:18:24 +0530

    updated

--- src/pages/LoginRegister.jsx
+++ src/pages/LoginRegister.jsx
@@ -11,7 +11,7 @@ const LoginRegister = () => {
                 {/*container */}
                 <div className='w-11/12 max-w-3xl min-h-[500px] flex flex-col md:flex-row lg:flex-row bg-white rounded-xl mt-14 md:mt-0'>
                     {/*sign in section  */}
-                    <div className='w-full md:w-1/2 lg:w-1/2 p-6 flex flex-col justify-center items-center bg-violet-500 rounded-r-none md:rounded-r-[150px] rounded-l-xl space-y-6'>
+                    <div className='w-full md:w-1/2 lg:w-1/2 p-6 flex flex-col justify-center items-center bg-violet-500 rounded-t-xl md:rounded-r-[150px]  space-y-6'>
                         <h1 className='text-3xl text-white font-semibold'>Welcome back!</h1>
                         <p className='text-sm text-white text-center'>Enter your Personal details to used all of site <br className='hidden md:block ' /> features</p>
                         <button className='px-8 py-1 rounded-md border text-white border-white bg-violet-500 hover:bg-white hover:text-violet-500 hover:border-violet-500'>SIGN IN </button>
@@ -43,10 +43,10 @@ const LoginRegister = () => {
                         <h1 className='text-center text-sm text-gray-600 '>Or Use your email for registration</h1>
                         {/* form section */}
                         <form className='flex flex-col w-full space-y-5'>
-                            <input className='px-4 py-3 outline-none rounded-lg border border-gray-200 shadow focus:ring ring-violet-3-4' type="text" placeholder='name' />
+                            <input className='px-4 py-3 outline-none rounded-lg border border-gray-200 shadow focus:ring ring-violet-300' type="text" placeholder='name' />
                             <input className='px-4 py-3 outline-none rounded-lg border border-gray-200 shadow focus:ring ring-violet-300' type="email" placeholder='Email' />
                             <input className='px-4 py-3 outline-none rounded-lg border border-gray-200 shadow focus:ring ring-violet-300' type="password" placeholder='Password'  />
-                            <button className='px-4 py-3 bg-violet-500 rounded-lg text-white'>SIGN UP</button>
+                            <button className='px-4 py-3 bg-violet-500 rounded-lg border text-white hover:bg-white hover:text-violet-600 hover:border-violet-600 cursor-pointer  '>SIGN UP</button>
                         </form>
 
                     </div>
